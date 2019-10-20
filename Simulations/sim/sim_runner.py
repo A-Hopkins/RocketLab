ca959edac460b1e6ca2322314259ed6bcc118fce
Alex Hopkins <hopkinsalex@knights.ucf.edu>
2019-10-20 15:56:25 -0400

    Main GUI Windows added. Need to add connections to the simulations

--- Simulations/sim/sim_runner.py
+++ Simulations/sim/sim_runner.py
@@ -55,44 +55,47 @@ class Window(tkinter.Frame):
         window.title("Load configurations")
         window.geometry("330x420")
 
-        # Table text, need a better implementation
-        # TODO: Fix bug validate command only works once
-        # TODO: Add implementation beyond print value
+        # Table text, need a better implementation Maybe save string vals in a list to iterate through the table entries
+        # And assign the values to the basic entry windows?
         table_entry1 = tkinter.Label(window, text="Dry Mass:")
         table_entry1.grid(row=0, column=0)
         dry_mass = tkinter.StringVar()
-        table_entry2 = tkinter.Entry(window, textvariable=dry_mass, validate="focusout",
-                                     validatecommand=lambda: print(dry_mass.get()))
+        table_entry2 = tkinter.Entry(window, textvariable=dry_mass)
 
         table_entry2.grid(row=0, column=1)
 
         table_entry3 = tkinter.Label(window, text="Motor mass:")
         table_entry3.grid(row=1, column=0)
         motor_mass = tkinter.StringVar()
-        table_entry4 = tkinter.Entry(window, textvariable=motor_mass, validate="focusout",
-                                     validatecommand=lambda: print(motor_mass.get()))
+        table_entry4 = tkinter.Entry(window, textvariable=motor_mass)
         table_entry4.grid(row=1, column=1)
 
         table_entry5 = tkinter.Label(window, text="Impulse:")
         table_entry5.grid(row=2, column=0)
         impulse = tkinter.StringVar()
-        table_entry6 = tkinter.Entry(window, textvariable=impulse, validate="focusout",
-                                     validatecommand=lambda: print(impulse.get()))
+        table_entry6 = tkinter.Entry(window, textvariable=impulse)
         table_entry6.grid(row=2, column=1)
 
         table_entry7 = tkinter.Label(window, text="Max Thrust:")
         table_entry7.grid(row=3, column=0)
         max_thrust = tkinter.StringVar()
-        table_entry8 = tkinter.Entry(window, textvariable=max_thrust, validate="focusout",
-                                     validatecommand=lambda: print(max_thrust.get()))
+        table_entry8 = tkinter.Entry(window, textvariable=max_thrust)
         table_entry8.grid(row=3, column=1)
 
         table_entry9 = tkinter.Label(window, text="2nd Engine Fire Time:")
         table_entry9.grid(row=4, column=0)
         engine_fire_time = tkinter.StringVar()
-        table_entry10 = tkinter.Entry(window, textvariable=engine_fire_time, validate="focusout",
-                                      validatecommand=lambda: print(engine_fire_time.get()))
+        table_entry10 = tkinter.Entry(window, textvariable=engine_fire_time)
         table_entry10.grid(row=4, column=1)
+
+        # Load button
+        load_button = tkinter.Button(window, text="Load!", command=lambda: load_config(dry_mass.get(),
+                                                                                       motor_mass.get(),
+                                                                                       impulse.get(),
+                                                                                       max_thrust.get(),
+                                                                                       engine_fire_time.get()
+                                                                                       ))
+        load_button.place(relx=0.5, rely=0.75, anchor=tkinter.CENTER)
 
     def simulate_window(self):
         """
@@ -107,12 +110,45 @@ class Window(tkinter.Frame):
 
         combo_box_value = tkinter.StringVar()
         combo_box = tkinter.ttk.Combobox(window, textvariable=combo_box_value)
-        # TODO: Do more than print value
-        combo_box.bind("<<ComboboxSelected>>", lambda get_value: print(combo_box.get()))
+        combo_box.bind("<<ComboboxSelected>>", lambda event: graph_type(combo_box.get()))
         combo_box['values'] = ('Height vs Acceleration', 'Velocity vs Time', 'Acceleration vs Time')
 
         combo_box.current(0)
         combo_box.place(relx=0.30, rely=0.95, anchor=tkinter.SE)
+
+
+def graph_type(combo_box_value):
+    """
+    Returns the graph type from the combo box window selector.
+
+    :param combo_box_value: Value of the selected box
+
+    :return graph_value: The value to set the graph to
+    """
+    # TODO: Load associated graph png onto the display
+    graph_value = combo_box_value
+    print(graph_value)
+
+    return graph_value
+
+
+def load_config(dry_mass, motor_mass, impulse, max_thrust, engine_fire_time):
+    """
+    Loads the Entry values and returns the fields in a list.
+
+    :param dry_mass: Dry mass from the Entry Field on the GUI
+    :param motor_mass: Motor Mass from the Entry Field on the GUI
+    :param impulse: Impulse text from the Entry Field on the GUI
+    :param max_thrust: Max thrust text from the Entry Field on the GUI
+    :param engine_fire_time: Second Engine Firing time from the Entry Field on the GUI
+
+    :return config_list: List of parameter values
+    """
+    # TODO: Put these values into the simulation
+    print(dry_mass, motor_mass, impulse, max_thrust, engine_fire_time)
+    config_list = [dry_mass, motor_mass, impulse, max_thrust, engine_fire_time]
+
+    return config_list
 
 
 def run():
